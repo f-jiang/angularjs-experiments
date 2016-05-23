8d07851577029b0c70f10d9da822efa75f3f413a
Feilan Jiang <feilanjiang@gmail.com>
2016-05-22 22:27:47 -0400

    Init commit

---
+++ scope/app.js
@@ -0,0 +1,12 @@
+var app = angular.module('app', []);
+
+app.controller('controller', function($scope) {
+	$scope.names = [
+		"Kyle	Schultz",
+		"Geraldine	Henderson",
+		"Herman	Romero",
+		"Allan	Terry",
+		"Rochelle	Hill"
+	];
+});
+
